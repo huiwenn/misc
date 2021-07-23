46c2ac54e0c8c765c7ebc1ab3b4c3d7902c12c6a
Sophia Sun <sophiaphia@live.com>
2021-07-23 03:59:41 -0700

    trainscript

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-install torch
+echo yes | conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch 
 python scripts/train.py --dataset_path /data/argoverse --rho-reg --model_name rho_reg_pecco --batch_size 16 --val_batch_size 32 --train --model_name cloud --batches_per_epoch 600 --val_batches 50 --epochs 100 --loss ecco
