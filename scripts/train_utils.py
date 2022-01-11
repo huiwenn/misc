7b3769eeb313efc0f1a0f1122515836230df0791
sophia <sophiaphia@live.com>
2022-01-11 01:05:00 -0800

    debug ctsconv

--- scripts/train_utils.py
+++ scripts/train_utils.py
@@ -200,7 +200,6 @@ def process_batch_ped(batch, device, train_window = 12, train_particle_num=40):
     # accel = torch.zeros(batch_size, 1, 2).to(device)
     batch_tensor['accel'] = accel
     batch_size = batch_tensor['pos0'].shape[0]
-    print(batch_tensor['pos0'].shape)
     batch_tensor['sigmas'] = torch.zeros(batch_size, train_particle_num, 2, 2).to(device)
 
     return batch_tensor
